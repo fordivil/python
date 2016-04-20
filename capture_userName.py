b752b3fe816e78bb6b72305c42b64453e76c326d
fordivil <fordivil1@gmail.com>
2016-04-20 14:40:50 +0900

    Update capture_userName.py

--- capture_userName.py
+++ capture_userName.py
@@ -1,12 +1,13 @@
 from PIL import Image
+import cv2
 
 def capture_userName():
     img = Image.open("1234.png", mode='r')
 
     x= 0
-    a = 110
+    a = 114
     b = 120
-    c = 250
+    c = 235
     d = 145
 
     z=80
@@ -24,7 +25,16 @@ def capture_userName():
                 d
             )
         )
-        iimg.save("Mname"+str(x)+".png")
+
+
+        iimg.save("Mname33"+str(x)+".png")
+        image = cv2.imread("Mname33"+str(x)+".png")
+        r = 1000.0 / image.shape[1] # 가로세로배율
+        dim = (1000, int(image.shape[0] * r)) #가로세로배율
+        resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+       # cv2.imshow("scaling",resized)
+       # cv2.waitKey(0)
+        cv2.imwrite("Mname33"+str(x)+".png",resized)
         x+=1
 
 
